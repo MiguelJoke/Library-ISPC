18873ddebc3d896b0adc8fd44ee38460b9a79ef4
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-09-28 19:29:14 -0300

    cambios en los cors

--- Backend en SpringBoot/src/main/java/com/ispc/library/controller/UserController.java
+++ Backend en SpringBoot/src/main/java/com/ispc/library/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Caro
  */
-@CrossOrigin
+@CrossOrigin ( origins = "http://localhost:4200/")
 @RestController
 public class UserController {
    
